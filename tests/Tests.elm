6580c6340cddf118e72530b4944fb69c24689722
Bogdan Zaharia <zaboco@gmail.com>
2016-11-09 00:45:07 +0200

    add tests for more complex drag scenarios

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,7 +1,8 @@
 module Tests exposing (..)
 
-import Fuzz exposing (Fuzzer, int)
+import Fuzz exposing (Fuzzer, int, list)
 import Mouse exposing (Position)
+import Return exposing (Return)
 import Test exposing (..)
 import Expect as Should exposing (Expectation)
 import Draggable as D
@@ -36,7 +37,39 @@ all =
                 Dragging lastPosition
                     |> D.update DragEnd
                     |> Should.equal ( NoDrag, Cmd.none )
+        , fuzz3 positionF dragUpdatesF positionF "multi DragAt records last position" <|
+            \firstPosition middleDragUpdates lastPosition ->
+                Return.singleton NoDrag
+                    |> andThen (D.update << DragStart <| firstPosition)
+                    |> andThenAll middleDragUpdates
+                    |> andThen (D.update << DragAt <| lastPosition)
+                    |> shouldYield (Dragging lastPosition)
+        , fuzz2 positionF dragUpdatesF "complete drag ends up in NoDrag" <|
+            \firstPosition middleDragUpdates ->
+                Return.singleton NoDrag
+                    |> andThen (D.update << DragStart <| firstPosition)
+                    |> andThenAll middleDragUpdates
+                    |> andThen (D.update DragEnd)
+                    |> shouldYield NoDrag
         ]
+
+
+
+-- Fuzzers
+
+
+dragUpdatesF : Fuzzer (List (Model -> Return Msg Model))
+dragUpdatesF =
+    list <| Fuzz.map (D.update << DragAt) <| positionF
+
+
+positionF : Fuzzer Position
+positionF =
+    Fuzz.map2 Position int int
+
+
+
+-- Expectation Helpers
 
 
 shouldYield : model -> ( model, Cmd msg ) -> Expectation
@@ -44,6 +77,15 @@ shouldYield expected ( actual, _ ) =
     Should.equal expected actual
 
 
-positionF : Fuzzer Position
-positionF =
-    Fuzz.map2 Position int int
+
+-- Return Helpers
+
+
+andThenAll : List (a -> Return msg a) -> Return msg a -> Return msg a
+andThenAll fns initial =
+    fns |> List.foldl andThen initial
+
+
+andThen : (a -> Return msg b) -> Return msg a -> Return msg b
+andThen =
+    flip Return.andThen
