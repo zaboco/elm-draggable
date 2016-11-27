c770bcbd772136a066b9bd8f5b1a6837acb7b3da
Bogdan Zaharia <zaboco@gmail.com>
2016-11-27 12:57:10 +0200

    fix constraints.html styling

--- build-examples.sh
+++ build-examples.sh
@@ -3,5 +3,5 @@
 cd examples/
 elm-make BasicExample.elm --output ../basic.html
 elm-make CustomEventsExample.elm --output ../custom.html
-elm-make ConstraintsExample.elm --output ../constraints.html
+elm-make ConstraintsExample.elm --output ../constraints.js
 elm-make PanAndZoomExample.elm --output ../pan-and-zoom.html
