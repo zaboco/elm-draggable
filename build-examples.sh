194971777aeb2b9796e6363e3c23a5d71f912963
Bogdan Zaharia <zaboco@gmail.com>
2016-11-27 12:30:42 +0200

    compile examples and add script for that

---
+++ build-examples.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cd examples/
+elm-make BasicExample.elm --output ../basic.html
+elm-make CustomEventsExample.elm --output ../custom.html
+elm-make ConstraintsExample.elm --output ../constraints.html
+elm-make PanAndZoomExample.elm --output ../pan-and-zoom.html
