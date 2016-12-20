bde7392e48b5a29ab267aeb2e13f9cefe060a304
Bogdan Zaharia <zaboco@gmail.com>
2016-12-20 21:59:30 +0200

    replace js example files with htmls

--- build-examples.sh
+++ build-examples.sh
@@ -3,6 +3,6 @@
 cd examples/
 elm-make BasicExample.elm --output ../basic.html
 elm-make CustomEventsExample.elm --output ../custom.html
-elm-make ConstraintsExample.elm --output ../constraints.js
+elm-make ConstraintsExample.elm --output ../constraints.html
 elm-make PanAndZoomExample.elm --output ../pan-and-zoom.html
-elm-make MultipleTargetsExample.elm --output ../multiple.js
+elm-make MultipleTargetsExample.elm --output ../multiple.html
